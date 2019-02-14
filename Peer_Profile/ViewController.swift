33b8d7af96e5f809717e844a1717513bd96cdfc3
yazuyazuya <kzyono-1999@docomo.ne.jp>
2019-02-14 10:52:25 +0900

    Initial Commit

---
+++ Peer_Profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Peer_Profile
+//
+//  Created by 大野和也 on 2019/02/14.
+//  Copyright © 2019 yazuyazuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Peer_ProfileTests/Peer_ProfileTests.swift
@@ -0,0 +1,34 @@
+//
+//  Peer_ProfileTests.swift
+//  Peer_ProfileTests
+//
+//  Created by 大野和也 on 2019/02/14.
+//  Copyright © 2019 yazuyazuya. All rights reserved.
+//
+
+import XCTest
+@testable import Peer_Profile
+
+class Peer_ProfileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
